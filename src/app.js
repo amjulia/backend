afe5d95d5cbbbfc0ac8747eb6e484d6b403a72fc
Karpova Julia <am_julia@mail.ru>
2024-07-17 12:05:39 +0600

    status

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const server = http.createServer((request, response) => {
   const url = new URL(request.url, ipAddress);
   const userName = url.searchParams.get("hello"); // тут и будет имя храниться
   if (userName) {
-    response.status = 200;
+    response.statusCode = 200;
     response.statusMessage = "Ok";
     response.header = "Content-Type: text/plain";
     response.write(`Hello, ${userName}.`);
@@ -15,7 +15,7 @@ const server = http.createServer((request, response) => {
     return;
   }
   if (request.url === "/?hello"){
-    response.status = 400;
+    response.statusCode = 400;
     response.statusMessage = "Bad Request";
     response.header = "Content-Type: text/plain";
     response.write("Enter a name");
@@ -25,7 +25,7 @@ const server = http.createServer((request, response) => {
   
 
   if (request.url === "/?users") {
-    response.status = 200;
+    response.statusCode = 200;
     response.statusMessage = "Ok";
     response.header = "Content-Type: application/json";
     response.write(getUsers());
@@ -33,17 +33,19 @@ const server = http.createServer((request, response) => {
     return;
   }
   if (request.url === "/") {
-    response.status = 200;
+    response.statusCode = 200;
     response.statusMessage = "Ok";
     response.header = "Content-Type: text/plain";
     response.write("Hello, world!");
     response.end();
+    return
   }
     
- 
-  response.status = 500;
+ response.statusCode = 500;
   response.statusMessage = "Internal Server Error";
   response.end();
+
+  
 });
 server.listen(3003, () => {
   console.log("Сервер запущен по запросу http://127.0.0.1:3003");
